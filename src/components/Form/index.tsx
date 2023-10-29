85f87d56d49bbb6144d1b32a6accf227ceedd3d0
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-10-29 12:24:06 -0300

    Continuação do desenvolvimento do componente Formulário.

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import style from "./Form.module.scss";
 
 const Form = () => {
+  const [estudos, setEstudos] = useState({
+    tarefa: "",
+    tempo: "00:00:00",
+  });
+
+  const adicionarEstudo = (evento: React.FormEvent) => {
+    evento.preventDefault();
+    console.log("estudos: ", estudos);
+  };
+
   return (
-    <form className={style.novaTarefa}>
+    <form className={style.novaTarefa} onSubmit={adicionarEstudo}>
       <div className={style.inputContainer}>
         <label htmlFor="task">Adicione um novo estudo</label>
         <input
           type="text"
           name="task"
           id="task"
+          value={estudos.tarefa}
+          onChange={(evento) =>
+            setEstudos({ ...estudos, tarefa: evento.target.value })
+          }
           placeholder="O que você quer estudar?"
           required
         />
@@ -21,6 +35,10 @@ const Form = () => {
           type="time"
           step="1"
           name="time"
+          value={estudos.tempo}
+          onChange={(evento) =>
+            setEstudos({ ...estudos, tempo: evento.target.value })
+          }
           id="time"
           min="00:00:00"
           max="01:30:00"
