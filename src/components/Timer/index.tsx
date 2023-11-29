3b87479d64204904ec4d06e4a218907234d40be3
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-11-29 13:23:20 -0300

    Testando a aplicação da lógica para converter tempo em segundos.

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./Timer.module.scss";
+import { tempoParaSegundos } from "../../common/utils/date";
 
 const Timer = () => {
+  console.log("conversão: ", tempoParaSegundos("01:01:01"));
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
